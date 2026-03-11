80414a6f82829cc2bbba98db2bd7eed16acd0d1f
kumarmangalamsingh <kumarmangalamsingh040@gmail.com>
2026-03-11 22:38:04 +0530

    few changes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,28 +1,8 @@
-import { Github, GithubIcon, LinkedinIcon, Mail, MapIcon, MapPin, Phone, Send, Twitter } from "lucide-react"
+import { Mail, Phone, Send } from "lucide-react";
 import { useState } from "react";
 
-export const Contact=()=>{
-    const [result,setResult]=useState("");
-    const formsubmit= async(event)=>{
-        event.preventDefault();
-        setResult("Sending...");
-        const formdata=new FormData(event.target);
-        formdata.append("access_key", "92786123-f172-4560-aa28-42bd6573ad44");
-
-        const response=await fetch("https://api.web3forms.com/submit",{
-            method:"POST",
-            body:formdata
-        });
-        const data=await response.json();
-
-        if(data.success){
-            setResult("Form submitted successfully");
-            event.target.reset();
-        }
-        else{
-            console.log("Error",data);
-            setResult(data.message);
-        }
+export const Contact = () => {
+  const [result, setResult] = useState("");
 
   const formsubmit = async (event) => {
     event.preventDefault();
@@ -90,18 +70,15 @@ export const Contact=()=>{
               </a>
             </div>
 
-            <div className="space-y-6 justify-center">
-                <div className="flex items-start space-x-2">
-                    <div className="p-3 rounded-full bg-primary/10">
-                      <GithubIcon  className="h-6 w-6 text-primary"/>
-                    </div>
-                    <div>
-                <a href="https://github.com/KumarMangalamSingh"
-                target="_blank" className="text-muted-foreground hover:text-primary transition-color">KumarMangalamSingh</a>
-                </div>
-                </div>
-                
-                
+            {/* PHONE */}
+            <div className="flex items-start space-x-4">
+              <div className="p-3 rounded-full bg-primary/10">
+                <Phone className="h-6 w-6 text-primary" />
+              </div>
+
+              <span className="text-muted-foreground">
+                7903509112
+              </span>
             </div>
           </div>
 
@@ -169,34 +146,7 @@ export const Contact=()=>{
           </div>
 
         </div>
-        <div className="bg-card p-8 rounded-lg shadow-xs mt-8">
-                <h3 className="text-2xl font-semibold mb-6">Send a Message </h3>
-                <form className="space-y-6" onSubmit={formsubmit}>
-                    <label htmlFor="name">Your Name</label>
-                    <input type="text" id="name" name="name" required
-                    placeholder="Enter Name..." 
-                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focud:ring-primary"/>
-                </form>
-                <form className="space-y-6">
-                    <label htmlFor="name">Your Email</label>
-                    <input type="text" id="name" name="name" required
-                    placeholder="abc@gmail.com..." 
-                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focud:ring-primary"/>
-                </form>
-
-                <form className="space-y-6">
-                    <label htmlFor="name">Your Message</label>
-                    <textarea type="text" id="name" name="name" required
-                    placeholder="write the message here..." 
-                    className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focud:ring-primary"/>
-                </form>
-                <button onClick={formsubmit} className="cosmic-button w-full flex items-center justify-center gap-2" typeof="submit">
-                   Send <Send size={16} className="items-center"/>
-                </button>
-                <span>{result}</span>
-            </div>
-     </div>
-     </div>
+      </div>
     </section>
   );
 };
